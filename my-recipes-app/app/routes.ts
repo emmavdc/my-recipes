0778365a22271a87b2843cb8c16fc1820cd6cb82
Emma Vandecaasteele <emma_vandecasteele@yahoo.fr>
2026-04-12 18:42:39 +0200

    some tests :)

--- my-recipes-app/app/routes.ts
+++ my-recipes-app/app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/recipes._index.tsx"),
-    //route("recipe/:id", "routes/recipe.tsx"),
+    route("recipes/:id", "routes/recipe.tsx"),
+    //route("recipes/", "routes/recipes._index.tsx"),
 ] satisfies RouteConfig;
 
